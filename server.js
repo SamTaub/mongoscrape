bd18313ed11e27fd1dd7f9f445e333e2bb309d62
Sam Taub <staub91@gmail.com>
2019-03-25 16:17:20 -0400

    added handlebars functionality

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/', function (req, res) {
             };
             res.render("index", hbsObject);
         }).catch(function (err) {
-            console.log(err);
+            res.json(err);
         });
 });
 
@@ -59,7 +59,7 @@ app.get('/scrape', function (req, res) {
 
             let result = {};
             result.title = $(this).children('div.content').children('a').children('h5').text();
-            result.link = 'https://www.profootballfocus.com/' + $(this).children('div.content').children('a').attr('href');
+            result.link = $(this).children('div.content').children('a').attr('href');
             result.summary = $(this).children('div.content').children('p').text();
 
             db.Article.create(result)
