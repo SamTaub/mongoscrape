11e51e4e7f37a1e3ea7660361b001f409e227fe4
Sam Taub <staub91@gmail.com>
2019-03-25 12:35:07 -0400

    created comment model

---
+++ models/Comment.js
@@ -0,0 +1,17 @@
+//Require Mongoose module
+const mongoose = require('mongoose');
+
+//Create reference to the Mongoose Schema constructor
+const Schema = mongoose.Schema;
+
+//Define a Schema for Comments
+const CommentSchema = new Schema({
+    //Defines the body of the comment as a string
+    body: String
+});
+
+//Create model for Comments
+const Comment = mongoose.model("Comment", CommentSchema);
+
+//Export Comments model
+module.exports = Comment;
