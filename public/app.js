03c275b8d5836d0cd7d97b3010c42ca95859deba
Sam Taub <staub91@gmail.com>
2019-03-27 15:49:04 -0400

    comments now display

--- public/app.js
+++ public/app.js
@@ -1,39 +1,60 @@
 $(function () {
     //Save article to database on click
-    $('.btn-save').on("click", function(event){
+    $('.btn-save').on("click", function (event) {
         event.preventDefault();
         const id = $(this).attr('id');
 
         $.ajax({
             method: 'POST',
             url: `/api/saved/${id}`
-        }).then(function(){
+        }).then(function () {
             location.reload();
         });
     });
     //Remove saved articles on click
-    $('.btn-remove-saved').on("click", function(event){
+    $('.btn-remove-saved').on("click", function (event) {
         event.preventDefault();
         const id = $(this).attr('data');
 
         $.ajax({
             method: 'POST',
             url: `/api/removed/${id}`
-        }).then(function(){
+        }).then(function () {
             location.reload();
         });
     });
     //Open comment modal
-    $('.btn-comment').on("click", function(){
+    $('.btn-comment').on("click", function () {
         const id = $(this).attr('id');
         $.ajax({
             method: 'GET',
             url: `/articles/${id}`
-        }).then(function(data){
-            console.log(data);
+        }).then(function (data) {
+            $(".btn-save-comment").attr("data", data._id)
+            if (data.comment) {
+                $(".commentInput").val(data.comment.body);
+            }
         })
         $('#commentModal').modal('toggle');
     });
+    //Save comments
+    $(".btn-save-comment").on("click", function (){
+        const id = $(this).attr('data');
+        console.log(id);
+        const comment = $(".commentInput").val();
+        $.ajax({
+            method: 'POST',
+            url: `/comments/${id}`,
+            data:{body: comment}
+        }).then(function(data){
+            console.log(data)
+            console.log(comment);
+            location.reload();
+        })
+        $(".commentInput").val("");
+        $("#commentModal").modal('toggle');
+    });
+
 });
 
 
--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.set("view engine", "handlebars");
 
 //Scrape profootballfocus.com
 app.get('/scrape', function (req, res) {
-    
+
     axios.get('https://www.profootballfocus.com/').then(function (response) {
 
         const $ = cheerio.load(response.data);
@@ -74,14 +74,24 @@ app.get('/api/articles', function (req, res) {
         });
 });
 
+//API Page for comments
+app.get('/comments', function (req, res) {
+    db.Comment.find({})
+        .then(function (dbComment) {
+            res.json(dbComment)
+        }).catch(function (err) {
+            res.json(err);
+        });
+});
+
 //API Page for saved articles
-app.get('/api/saved', function(req, res){
-    db.Article.find({saved: true})
-    .then(function(savedArticle){
-        res.json(savedArticle);
-    }).catch(function (err) {
-        res.json(err);
-    });
+app.get('/api/saved', function (req, res) {
+    db.Article.find({ saved: true })
+        .then(function (savedArticle) {
+            res.json(savedArticle);
+        }).catch(function (err) {
+            res.json(err);
+        });
 });
 
 //Render articles on homepage
@@ -98,52 +108,71 @@ app.get('/', function (req, res) {
 });
 
 //Render saved articles on /saved
-app.get('/saved', function(req, res){
-    db.Article.find({saved: true})
-    .then(function(savedArticle){
-        const hbsObject = {
-            article: savedArticle
-        };
-        res.render("saved", hbsObject);
-    }).catch(function (err) {
-        res.json(err);
-    });
+app.get('/saved', function (req, res) {
+    db.Article.find({ saved: true })
+        .then(function (savedArticle) {
+            const hbsObject = {
+                article: savedArticle
+            };
+            res.render("saved", hbsObject);
+        }).catch(function (err) {
+            res.json(err);
+        });
 });
 
 //Save articles to saved API and update them in Mongo DB
-app.post('/api/saved/:id', function (req, res){
-    db.Article.findOneAndUpdate({_id: req.params.id}, {saved: true})
-    .then(function (article){
-        res.json(article)
-    }).catch(function (error){
-        res.json(error)
-    });
+app.post('/api/saved/:id', function (req, res) {
+    db.Article.findOneAndUpdate({ _id: req.params.id }, { saved: true })
+        .then(function (article) {
+            res.json(article)
+        }).catch(function (error) {
+            res.json(error)
+        });
 });
 //Create a dummy route for deleted articles
-app.post('/api/removed/:id', function (req, res){
-    db.Article.findOneAndUpdate({_id: req.params.id}, {saved: false})
-    .then(function (article){
-        res.json(article)
-    }).catch(function (error){
-        res.json(error)
-    });
+app.post('/api/removed/:id', function (req, res) {
+    db.Article.findOneAndUpdate({ _id: req.params.id }, { saved: false })
+        .then(function (article) {
+            res.json(article)
+        }).catch(function (error) {
+            res.json(error)
+        });
 });
 
-app.get("/articles/:id", function(req, res) {
+
+app.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
-      .populate("Comment")
-      .then(function(dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+        // ..and populate all of the notes associated with it
+        .populate("comment")
+        .then(function (dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
+app.post("/comments/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Comment.create(req.body)
+        .then(function (dbComment) {
+            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
 
 //Start server
